6532ce9fb16d1dfe96022eb772542006f838db73
DaniBoyzin <bigdino451@gmail.com>
2025-11-27 23:39:37 -0300

    .

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
-criarCartao(categoria, pergunta, resposta) {
-    console.log(categora, pergunta, resposta)
-}
+function criarCartao(categoria, pergunta, resposta) {
+    let container = document.getElementById('container')
+    let cartao = document.createElement('article')
+    cartao.className = 'cartao'
+
+    cartao.innerHTML = `
+<div class="cartao_conteudo">
+<h3>${categoria}</h3>
+<div class="cartao_conteudo_pergunta">
+<p>${pergunta}/p>
+</div>
+<div class="cartao_conteudo_resposta">
+<p>${resposta}</p>
+</div>
+</div>
+`
+  container.appendChild(cartao)
+
+    let respostaEstaVisivel = false
+    function viraCartao(){
+  respostaEstaVisivel = !respostaEstaVisivel
+  cartao.classList.toggle('ative', respostaEstaVisivel)
+    }
+    cartao.addEventListener('click', viraCartao)
---
+++ asjhd.js
@@ -0,0 +1,30 @@
+criaCartao(
+    categoria,
+    pergunta,
+    resposta,
+)
+criaCartao(
+    'Dead By Daylight - Pergunta 1:',
+    'Qual é o primeiro boss do jogo?',
+    'O porteiro'
+)
+criaCartao(
+    'Pergunta 2:',
+    'O que é o protagonista de Dead cells ?',
+    'Um Homúnculo.',
+)
+criaCartao(
+    'Pergunta 3:',
+    'Qual a dificuldade máxima do jogo?',
+    '5 células',
+)
+criaCartao(
+    'Pergunta 4:',
+    'Qual o boss final verdadeiro?',
+    'É seg',
+)
+criaCartao(
+    'Pergunta 5:',
+    'Quem era o rei?',
+    'O próprio protagonista',
+)
